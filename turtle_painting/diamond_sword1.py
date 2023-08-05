fe7ca38e3b583b071473587a2aabe02c8fe4a14c
Flash_Dragons <132655963+FlashDragons@users.noreply.github.com>
2023-08-05 12:25:25 +0800

    Uploading files for the first time.
    Some of my preview works.

---
+++ turtle_painting/diamond_sword1.py
@@ -0,0 +1,683 @@
+from turtle import *
+colormode(255)
+hideturtle()
+
+penup()
+goto(-40, 20)
+pendown()
+pencolor(14, 63, 54)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(-20, 40)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(0, 60)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(20, 80)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(40, 100)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(60, 120)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(80, 140)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(100, 160)
+pendown()
+pensize(1)
+forward(60)
+right(90)
+forward(20)
+right(90)
+forward(60)
+right(90)
+forward(20)
+
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(140, 140)
+pendown()
+begin_fill()
+pencolor(8, 37, 32)
+right(90)
+forward(20)
+right(90)
+forward(40)
+right(90)
+forward(20)
+right(90)
+forward(40)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(120, 80)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(100, 60)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(80, 40)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(60, 20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(40, 0)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(20, -20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(0, -40)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(-20, -60)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(-60, -20)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(0, -80)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(20, -100)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(0, -120)
+pendown()
+begin_fill()
+forward(20)
+right(90)
+forward(40)
+right(90)
+forward(20)
+right(90)
+forward(40)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(0, -100)
+pendown()
+begin_fill()
+for i in range(2):
+    forward(40)
+    right(90)
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(-40, -80)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(-100, -140)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(-100, -160)
+pendown()
+begin_fill()
+for i in range(2):
+    forward(60)
+    right(90)
+    forward(20)
+    right(90)
+color(8, 37, 32)
+end_fill()
+
+penup()
+goto(-60, 0)
+pendown()
+begin_fill()
+pencolor(14, 63, 54)
+for i in range(4):
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-100, 0)
+pendown()
+begin_fill()
+for i in range(2):
+    forward(20)
+    right(90)
+    forward(40)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-80, 20)
+pendown()
+begin_fill()
+pencolor(14, 63, 54)
+for i in range(4):
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-80, -40)
+pendown()
+begin_fill()
+for i in range(2):
+    forward(20)
+    right(90)
+    forward(40)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-60, -60)
+pendown()
+begin_fill()
+pencolor(14, 63, 54)
+for i in range(4):
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-140, -140)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-120, -120)
+pendown()
+begin_fill()
+for i in range(2):
+    forward(40)
+    right(90)
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-80, 0)
+pendown()
+pencolor(21, 99, 85)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(21, 99, 85)
+end_fill()
+
+penup()
+goto(-40, -40)
+pendown()
+pencolor(30, 138, 119)
+begin_fill()
+for i in range(2):
+    forward(40)
+    right(90)
+    forward(20)
+    right(90)
+color(30, 138, 119)
+end_fill()
+
+penup()
+goto(-60, -20)
+pendown()
+pencolor(30, 138, 119)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(30, 138, 119)
+end_fill()
+
+penup()
+goto(-120, -140)
+pendown()
+pencolor(21, 99, 85)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(21, 99, 85)
+end_fill()
+
+penup()
+goto(-40, -60)
+pendown()
+pencolor(21, 99, 85)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(21, 99, 85)
+end_fill()
+
+penup()
+goto(20, -100)
+pendown()
+pencolor(14, 63, 54)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(0, -80)
+pendown()
+pencolor(14, 63, 54)
+begin_fill()
+for i in range(2):
+    forward(40)
+    right(90)
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-20, -60)
+pendown()
+pencolor(14, 63, 54)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(14, 63, 54)
+end_fill()
+
+penup()
+goto(-20, -40)
+pendown()
+pencolor(21, 99, 85)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(21, 99, 85)
+end_fill()
+
+penup()
+goto(0, -40)
+pendown()
+pencolor(51, 235, 203)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(20, -20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(20, -20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(40, 0)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(60, 20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(80, 40)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(100, 60)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(51, 235, 203)
+end_fill()
+
+penup()
+goto(-20, -20)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(0, 0)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(20, 20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+color(51, 235, 203)
+end_fill()
+
+penup()
+goto(0, -20)
+pendown()
+pencolor(30, 138, 119)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(30, 138, 119)
+end_fill()
+
+penup()
+goto(20, 0)
+pendown()
+pencolor(43, 199, 172)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(40, 20)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(60, 40)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(80, 60)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(100, 80)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(120, 100)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(43, 199, 172)
+end_fill()
+
+penup()
+goto(40, 40)
+pendown()
+pencolor(164, 253, 240)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(60, 60)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(80, 80)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(100, 100)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(120, 120)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(164, 253, 240)
+end_fill()
+
+penup()
+goto(120, 80)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(140, 100)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(164, 253, 240)
+end_fill()
+
+penup()
+goto(140, 120)
+pendown()
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(164, 253, 240)
+end_fill()
+
+penup()
+goto(-100, -120)
+pendown()
+pencolor(104, 78, 30)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(104, 78, 30)
+end_fill()
+
+penup()
+goto(-60, -80)
+pendown()
+pencolor(104, 78, 30)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(104, 78, 30)
+end_fill()
+
+penup()
+goto(-100, -100)
+pendown()
+pencolor(73, 54, 21)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(-80, -80)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(73, 54, 21)
+end_fill()
+
+penup()
+goto(-80, -120)
+pendown()
+pencolor(40, 30, 11)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+
+penup()
+goto(-60, -100)
+pendown()
+for i in range(4):
+    forward(20)
+    right(90)
+color(40, 30, 11)
+end_fill()
+
+penup()
+goto(-80, -100)
+pendown()
+pencolor(137, 103, 39)
+begin_fill()
+for i in range(4):
+    forward(20)
+    right(90)
+color(137, 103, 39)
+end_fill()
+
+done()
